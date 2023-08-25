e983d3575fb56b4b2c8fba3279720dc11a1bdfed
CharlieRonchetti <85499184+CharlieRonchetti@users.noreply.github.com>
2023-08-25 16:59:32 +0100

    started mobile html layout

--- src/App.js
+++ src/App.js
@@ -3,36 +3,68 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Social Media Dashboard
-      Total Followers: 23,004
+      <header>
+        <div className="container header-content">
+        <h1>Social Media Dashboard</h1>
+        <p>Total Followers: 23,004</p>
+        <hr></hr>
+        </div>
+      </header>
 
-      Dark Mode
+      <div className="container theme">
+        <p>Dark Mode</p>
+        <div className="switcher">
+          <p>placeholder</p>
+        </div>
+      </div>
 
-      
+      <main>
+        <div className="container accounts">
+          <div className="facebook center">
+            <h2 className="username">@nathanf</h2>
+            <p className="follower-count">1987</p>
+            <p>FOLLOWERS</p>
+            <h3 className="daily-follows">12 Today</h3>
+          </div>
 
-      @nathanf
-      1987
-      Followers
-      12 Today
+          <div className="twitter center">
+            <h2 className="username">@nathanf</h2>
+            <p className="follower-count">1044</p>
+            <p>FOLLOWERS</p>
+            <h3 className="daily-follows">99 Today</h3>
+          </div>
 
-      @nathanf
-      1044
-      Followers
-      99 Today
+          <div className="instagram center">
+            <h2 className="username">@realnathanf</h2>
+            <p className="follower-count">11k</p>
+            <p>FOLLOWERS</p>
+            <h3 className="daily-follows">1099 Today</h3>
+          </div>
 
-      @realnathanf
-      11k
-      Followers
-      1099 Today
+          <div className="youtube center">
+            <h2 className="username">Nathan F.</h2>
+            <p className="follower-count">8239</p>
+            <p>Subscribers</p>
+            <h3 className="daily-follows">144 Today</h3>
+          </div>
+        </div>
 
-      Nathan F.
-      8239
-      Subscribers
-      144 Today
-
-
-
-      Overview - Today
+        <div className="container daily-overview">
+          <h2>Overview - Today</h2>
+          <div className="daily-stats">
+            <div className="card">
+              <div className="card-top-row row">
+                <h2 className="card-title">Page Views</h2>
+                <p>Logo Placeholder</p>
+              </div>
+              <div className="card-bottom-row row">
+                <p className="card-statistic">87</p>
+                <p>3%</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
 
       Page Views
       87
